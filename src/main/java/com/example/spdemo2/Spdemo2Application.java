0e7b40f0247b059de0e7a717ffc76892e8934af7
nairx <nairxprv@gmail.com>
2026-08-14 15:38:38 +0530

    first commit

---
+++ src/main/java/com/example/spdemo2/Spdemo2Application.java
@@ -0,0 +1,14 @@
+package com.example.spdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spdemo2Application.class, args);
+		System.out.println("My Application");
+	}
+
+}
